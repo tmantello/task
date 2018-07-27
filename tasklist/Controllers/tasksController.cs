efd4a4b108a8920e46f3753a812d9df3a0dcbd20
Tiago Mantello <tmantello@yahoo.com.br>
2018-07-26 22:31:53 -0300

    new
    enviando código

---
+++ tasklist/Controllers/tasksController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using tasklist.Models;
+
+namespace tasklist.Controllers
+{
+    public class tasksController : Controller
+    {
+        private tasklistEntities db = new tasklistEntities();
+
+        // GET: tasks
+        public ActionResult Index()
+        {
+            var tasks = db.tasks.Include(t => t.status);
+            return View(tasks.ToList());
+        }
+
+        // GET: tasks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            task task = db.tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // GET: tasks/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_status = new SelectList(db.status, "id", "descricao");
+            return View();
+        }
+
+        // POST: tasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,titulo,id_status,descricao,criacao,alteracao")] task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_status = new SelectList(db.status, "id", "descricao", task.id_status);
+            return View(task);
+        }
+
+        // GET: tasks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            task task = db.tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_status = new SelectList(db.status, "id", "descricao", task.id_status);
+            return View(task);
+        }
+
+        // POST: tasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,titulo,id_status,descricao,criacao,alteracao")] task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(task).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_status = new SelectList(db.status, "id", "descricao", task.id_status);
+            return View(task);
+        }
+
+        // GET: tasks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            task task = db.tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            task task = db.tasks.Find(id);
+            db.tasks.Remove(task);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
